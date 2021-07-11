3a847ea8dc6e8a34f6552d08bf0cc60bf5e85991
Ashley Follen <ashleyfollen@outlook.com>
2021-07-10 19:28:31 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const main = document.querySelector('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+victory.innerHTML = 'ASHLEY is the champion';
